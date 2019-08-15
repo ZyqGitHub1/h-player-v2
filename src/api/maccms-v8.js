9d48ad2f15d8c942fe43ed13b5fa038e6c6e66b0
zyq <qqdhsy@126.com>
2019-08-15 22:13:40 +0800

    add maccmsv8 api

--- src/api/maccms-v8.js
+++ src/api/maccms-v8.js
@@ -4,7 +4,6 @@ import { stringify } from 'query-string';
 export async function getList(api, query) {
   const defaultParams = {
     ac: 'list',
-    at: 'xml',
   };
   const params = Object.assign(defaultParams, query);
   const response = await axios.get(api, {
@@ -15,8 +14,7 @@ export async function getList(api, query) {
 
 export async function getDetail(api, query) {
   const defaultParams = {
-    ac: 'detail',
-    at: 'xml',
+    ac: 'videolist',
   };
   const params = Object.assign(defaultParams, query);
   const response = await axios.get(api, {
