24f66aeb3974089d4aa2a863b0adaae2af90c2a3
zyq <qqdhsy@126.com>
2019-08-18 15:17:50 +0800

    add rxjs utils

---
+++ src/utils/rx/parse-xml.js
@@ -0,0 +1,14 @@
+import { from } from 'rxjs';
+import util from 'util';
+import { parseString } from 'xml2js';
+
+const parseStringSync = util.promisify(parseString);
+
+export function parseXML(string, options) {
+  const encodeXml = string.replace(/&/g, '&amp;');
+  const defaultOptions = {
+    explicitArray: true,
+  };
+  const mergedOptions = Object.assign(defaultOptions, options);
+  return from(parseStringSync(encodeXml, mergedOptions));
+}
